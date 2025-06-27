e87600731df8afeeecd83c1b9e5a0320d5ecc3b0
NikhilReddy018 <nikhil2008reeddy898@gmail.com>
2025-06-27 22:32:46 +0530

    indexing

---
+++ practice_2.py
@@ -0,0 +1,6 @@
+a="Allu Arjun"
+print("given string is:",a)
+print("second occurance of A in backward direction:",a[-10])
+print("J in backward direction:",a[-3])
+print("second occurance of L in frontward dircton:",a[3])
+print("n in frontward direction:",a[10])
