ef387d983cc864fdf1155f8625d3cbda6785f837
NikhilReddy018 <nikhil2008reeddy898@gmail.com>
2025-06-27 22:41:57 +0530

    indexing

---
+++ practice_3.py
@@ -0,0 +1,7 @@
+a="Sony Bravia"
+print("given string is:",a)
+print("B in backward dirction:",a[-6])
+print("space in frontward dirction:",a[5])
+print("i in frontward direction:",a[10])
+print("last letter using frontward direction:",a[11])
+print("first letter using backward direction:",a[-11])
