23a70aa897e7cb04c123e8cbccf53aac00bc7bd4
NikhilReddy018 <nikhil2008reeddy898@gmail.com>
2025-06-27 22:17:49 +0530

    indexing

--- practice_1.py
+++ practice_1.py
@@ -1,3 +1,5 @@
 a=Harry potter
 print("given string is:",a)
-print("only space in backward direction:",a[-7])
+print("space in backward direction:",a[-7])
+print("first occurance of t in frontward direction:",a[8])
+print("second occurance of r in backward direction:",a[-1])
