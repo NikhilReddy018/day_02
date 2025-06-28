0b59aa28dddec376cb71ea1e9f4e5607080a0d7e
NikhilReddy018 <nikhil2008reeddy898@gmail.com>
2025-06-28 19:27:09 +0530

    indexing

--- practice_4.py
+++ practice_4.py
@@ -1,5 +1,5 @@
 a="Binladin"
 print("given string is:",a)
-print("middle letter l,a in backward,frontward direction:",a[-5],a[5])
+print("middle letter l,a in backward,frontward direction:",a[-5],a[4])
 print("second occurance of i in backward direction:",a[-7])
 print("first occurance of i in backward direction:",a[-2])
